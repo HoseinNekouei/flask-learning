1fecd0ad73b5e451d2fe15abf8e80a5515ba581b
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-07-13 20:07:37 +0330

    added render_template and define index file as main file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # This code sets up a simple Flask web application that returns "Hello, World!" when accessed at the root URL.
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def indexx():
-    return "Hello, World!"
+    return render_template('index.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)
