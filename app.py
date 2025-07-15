08396bc6327248d7638133b4e358347717346510
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-07-15 14:22:17 +0330

    Add placeholder comment for adding a new task in index route

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ App Initialization:
     - Runs the Flask development server in debug mode.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_scss import Scss
 from datetime import datetime
@@ -42,14 +42,32 @@ class MyTask(db.Model):
     def __repr__(self):
         return f"Task {self.id}"
     
-
-@app.route('/')
+# route to webpages
+# Homepage
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    # add a new task
+    if request.method == 'POST':
+        content = request.form['content']
+        new_task= MyTask(content=content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+        return redirect('/')
+
+    else:
+        # see all the current tasks
+        tasks= MyTask.query.order_by(MyTask.data_created.asc()  ).all()
+        return render_template('index.html', tasks=tasks)
+
+
 
 if __name__ == '__main__':
-    
+
     with app.app_context():
         db.create_all()
-    
-    app.run(debug=True)
